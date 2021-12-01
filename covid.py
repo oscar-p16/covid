d947d19768d1c0242c76af21796220a921a8b181
oscar p <oscarperezm16@hotmail.com>
2021-11-30 22:18:45 -0500

    n casos

--- covid.py
+++ covid.py
@@ -9,3 +9,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+url = 'covid_22_noviembre.csv'
+
+data = pd.read_csv(url)
+
+url = 'covid_22_noviembre.csv'
+
+data = pd.read_csv(url)
+
+#1 Número de casos de Contagiados en el País.
+num_pais = len(data)
+
+print("Punto 1")
+
+print(num_pais)
+
+print() 
